0cdd122bd1cf5817bda6cd27bc2e79095be58bcb
salma aitelabd <aitelabd@salmas-Laptop.local>
2023-05-22 23:45:46 +0100

    feat- links

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,22 @@
-export default function Home(){
-    return<h1>Home page 1</h1>
+import { execOnce } from "next/dist/shared/lib/utils"
+import Link from "next/link"
+
+  function Home(){
+    return(
+    <div>
+            <h1>Home page 1</h1>
+            <h2>
+                <Link  href='/docs'>
+                    Docs
+                </Link>
+            </h2>
+            <h2>
+                <Link   href='/product'>
+                    ProductList
+                </Link>
+            </h2>
+        </div>
+        )
 }
+
+export default Home
